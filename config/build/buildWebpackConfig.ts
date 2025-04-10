281ad51d07b761e07b6918fe6460671c1625fd4b
Roman Ponomarev <xgerfy@gmail.com>
2025-04-10 07:02:35 +0400

    add: devServer

--- config/build/buildWebpackConfig.ts
+++ config/build/buildWebpackConfig.ts
@@ -1,4 +1,5 @@
 import webpack from "webpack";
+import { buildDevServer } from "./buildDevServer";
 import { buildLoaders } from "./buildLoaders";
 import { buildPlugins } from "./buildPlugins";
 import { buildResolvers } from "./buildResolvers";
@@ -7,7 +8,7 @@ import { BuildOptions } from "./types/config";
 export function buildWebpackConfig(
   options: BuildOptions
 ): webpack.Configuration {
-  const { paths, mode } = options;
+  const { paths, mode, isDev } = options;
 
   return {
     mode: mode,
@@ -17,6 +18,8 @@ export function buildWebpackConfig(
       rules: buildLoaders(),
     },
     resolve: buildResolvers(),
+    devServer: isDev ? buildDevServer(options) : undefined,
+    devtool: isDev ? "inline-source-map" : undefined,
     output: {
       filename: "[name].[contenthash].js",
       path: paths.build,
--- src/test.ts
+++ src/test.ts
@@ -1,5 +1,5 @@
 export function someFn(arg: number): string {
-  console.log("random fn");
+  console.log("random fnsssss");
 
   return "";
 }
